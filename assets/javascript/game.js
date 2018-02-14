07ac256f36e762707be05cb42a55d38c0834bb2e
Ryan Mullowney <Batman@Ryans-MacBook-Pro.local>
2018-02-14 00:10:29 -0600

    Basic Javascript functionality added.  Game works for the most part.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,18 +11,63 @@
     // document.querySelector("#test").innerHTML = testArray.join(" ");
 
 
-
     // Establish variables
     var wordBank = ['eagle', 'tiger', 'elephant', 'moose', 'parakeet', 'turtle'];
-    var word = wordBank[Math.floor(Math.random()*wordBank.length)];
+    var word = wordBank[Math.floor(Math.random() * wordBank.length)];
     var wordSplit = [];
     var wordHangman = [];
-    
+    var guessed = [];
+    var guessesRemaining = 10;
+
     // Split the letters of the word up and add them to an array, and create array with letters hidden
     for (var i = 0; i < word.length; i++) {
         wordSplit.push(word[i])
         wordHangman.push("_")
     }
-    document.querySelector("#test").innerHTML = wordHangman.join(" ");
-    
+
+    // Presents the hidden word and guesses on the page
+    document.querySelector("#word").innerHTML = wordHangman.join(" ");
+    document.querySelector("#guessed").innerHTML = guessed.join(" ");
+    document.querySelector("#guessesRemaining").innerHTML = "Guesses remaining: " + guessesRemaining;
+
+
+    // Create function to detect keyboard input
+    document.onkeyup = function () {
+        // Store the player's input in a variable
+        var letter = String.fromCharCode(event.which).toLowerCase();
+
+        // Add the letter to the guessed list if it isn't already guessed
+        if (guessed.indexOf(letter) < 0) {
+            guessed.push(letter)
+            document.querySelector("#guessed").innerHTML = guessed.join(" ");
+
+            // Deduct guess from guessesRemaining only if it isn't a correct guess
+            if (wordSplit.indexOf(letter) < 0) {
+            document.querySelector("#guessesRemaining").innerHTML = "Guesses remaining: " + --guessesRemaining;
+            }
+        }
+
+        // Check whether player input is in word
+        for (var i = 0; i < wordSplit.length; i++) {
+            // If so, r
+            if (letter === wordSplit[i]) {
+                wordHangman[i] = letter
+
+                document.querySelector("#word").innerHTML = wordHangman.join(" ");
+
+            }
+        }
+
+        // Check whether player input is in word
+        // if (wordSplit.indexOf(letter) >= 0) {
+        //     console.log(wordSplit.indexOf(letter))
+        //     console.log(letter)
+
+
+    }
+
+
+
+
+
 })();
