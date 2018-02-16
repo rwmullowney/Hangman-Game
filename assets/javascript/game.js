568f57f4fa65f26dfc98052259177bd000e6ab94
Ryan Mullowney <Batman@Ryans-MacBook-Pro.local>
2018-02-16 12:29:03 -0600

    Completes basic game functionality.  Adds Nintendo logo to assets.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,70 +12,76 @@
 
 
     // Establish variables
-    var wordBank = ['eagle', 'tiger', 'elephant', 'moose', 'parakeet', 'turtle'];
-    var word = wordBank[Math.floor(Math.random() * wordBank.length)];
-    var wordSplit = [];
-    var wordHangman = [];
-    var guessed = [];
-    var guessesRemaining = 10;
 
-    // Split the letters of the word up and add them to an array, and create array with letters hidden
-    for (var i = 0; i < word.length; i++) {
-        wordSplit.push(word[i])
-        wordHangman.push("_")
+
+    function initGame() {
+        wordBank = ['eagle', 'tiger', 'elephant', 'moose', 'parakeet', 'turtle'];
+        word = wordBank[Math.floor(Math.random() * wordBank.length)];
+        wordSplit = [];
+        wordHangman = [];
+        guessed = [];
+        guessesRemaining = 10;
+
+        // Split the letters of the word up and add them to an array, and create array with letters hidden
+        for (var i = 0; i < word.length; i++) {
+            wordSplit.push(word[i])
+            wordHangman.push("_")
+        }
+        document.querySelector("#word").innerHTML = wordHangman.join(" ");
+        document.querySelector("#guessesRemaining").innerHTML = "Guesses remaining: " + guessesRemaining;
+        document.querySelector("#guessed").innerHTML = guessed.join(" ");
+        document.querySelector("#gameResult").innerHTML = "";
     }
 
-    // Presents the hidden word and guesses on the page
-    document.querySelector("#word").innerHTML = wordHangman.join(" ");
-    document.querySelector("#guessesRemaining").innerHTML = "Guesses remaining: " + guessesRemaining;
-    document.querySelector("#guessed").innerHTML = guessed.join(" ");
+    initGame()
+    playGame()
 
+    function playGame() {
+        // Create function to detect keyboard input
+        document.onkeyup = function () {
 
+            // Store the player's input in a variable, and store the keycode for that input in a variable too
+            var letter = String.fromCharCode(event.which).toLowerCase();
+            var keyCode = (event.keyCode ? event.keyCode : event.which);
+            if (guessesRemaining > 0 && wordSplit.toString() != wordHangman.toString()) {
+                // Add the letter to the guessed list if it isn't already guessed
+                if (guessed.indexOf(letter) < 0) {
+                    guessed.push(letter)
+                    document.querySelector("#guessed").innerHTML = guessed.join(", ");
 
-    // Create function to detect keyboard input
-    document.onkeyup = function () {
-        // Store the player's input in a variable
-        var letter = String.fromCharCode(event.which).toLowerCase();
+                    // Deduct guess from guessesRemaining only if it isn't a correct guess
+                    if (wordSplit.indexOf(letter) < 0) {
+                        document.querySelector("#guessesRemaining").innerHTML = "Guesses remaining: " + --guessesRemaining;
+                    }
+                }
 
-        // Add the letter to the guessed list if it isn't already guessed
-        if (guessed.indexOf(letter) < 0) {
-            guessed.push(letter)
-            document.querySelector("#guessed").innerHTML = guessed.join(", ");
+                // Check whether player input is in word
+                for (var i = 0; i < wordSplit.length; i++) {
+                    // If so, replace the hidden letter with the shown one
+                    if (letter === wordSplit[i]) {
+                        wordHangman[i] = letter
+                        document.querySelector("#word").innerHTML = wordHangman.join(" ");
+                    }
+                }
 
-            // Deduct guess from guessesRemaining only if it isn't a correct guess
-            if (wordSplit.indexOf(letter) < 0) {
-                document.querySelector("#guessesRemaining").innerHTML = "Guesses remaining: " + --guessesRemaining;
+                // Determine and display whether the player wins or loses
+                // Player wins
+                if (wordSplit.toString() === wordHangman.toString()) {
+                    window.setTimeout(function () {
+                        document.querySelector("#gameResult").innerHTML = "You win!  Press 'enter' to play again";
+                    }, 1);
+                // Player loses
+                } else if (guessesRemaining < 1 && wordSplit.toString() != wordHangman.toString()) {
+                    document.querySelector("#gameResult").innerHTML = "You lose!  Press 'enter' to play again";
+                }
+
+                // Reset the game when the player presses enter after winning/losing
+            } else {
+                if (keyCode === 13) {
+                    initGame()
+                }
             }
         }
-
-        // Check whether player input is in word
-        for (var i = 0; i < wordSplit.length; i++) {
-            // If so, replace the hidden letter with the shown one
-            if (letter === wordSplit[i]) {
-                wordHangman[i] = letter
-
-                document.querySelector("#word").innerHTML = wordHangman.join(" ");
-
-            }
-        }
-
-        // If guessesRemaining reaches 0 and the word is not complete, the user loses
-        if (wordSplit.toString() === wordHangman.toString()) {
-            alert("You win!  Play again?")
-            
-        }
-        else if (guessesRemaining < 1 && wordSplit.toString() != wordHangman.toString()) {
-            console.log("word split: " + wordSplit)
-            console.log("word Hangman: " + wordHangman)
-            alert("You lose!  Play again?")
-        } 
-
-
-
-
-
-
-
     }
 
 
