08999dcd724ba2b66171ca611f1784a09736932d
Ryan Mullowney <Batman@Ryans-MacBook-Pro.local>
2018-02-16 21:50:04 -0600

    Finished page, sans the sound effects and some extraneous features

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,7 +15,7 @@
 
 
     function initGame() {
-        wordBank = ['mario', 'pikachu', 'bowser', 'ganondorf', 'pikmin', 'dedede'];
+        wordBank = ['mario', 'pikachu', 'bowser', 'zelda', 'dedede'];
         word = wordBank[Math.floor(Math.random() * wordBank.length)];
         wordSplit = [];
         wordHangman = [];
@@ -67,21 +67,50 @@
                     window.setTimeout(function () {
                         document.querySelector("#gameResult").innerHTML = "You win!  Press 'enter' to play again";
                         // document.getElementById('myAudio').play();
+                        infoSwitch();
                     }, 1);
                 // Player loses
                 } else if (guessesRemaining < 1 && wordSplit.toString() != wordHangman.toString()) {
-                    document.querySelector("#gameResult").innerHTML = "You lose!  Press 'enter' to play again";
+                    document.querySelector("#gameResult").innerHTML = "You lose!  The word was '" + word + "'. \n Press 'enter' to play again";
+                    infoSwitch();
                 }
 
                 // Reset the game when the player presses enter after winning/losing
             } else {
                 if (keyCode === 13) {
-                    initGame()
+                    initGame();
                 }
             }
         }
     }
 
+    function infoSwitch(){
+        if (word === 'mario') {
+        document.querySelector("#infoImage").innerHTML = '<img class="img-fluid nintendo" src="assets/images/mario.png" alt="Mario jumping">';
+        document.querySelector("#infoText").innerHTML = "<p>Mario is the hero of the legendary 'Mario' series.  First introduced on the NES in 1985, Mario became a cultural icon and is now one of the most recognizable characters in the world.</p>";
+        }
+        if (word === 'zelda'){
+            document.querySelector("#infoImage").innerHTML = '<img class="img-fluid nintendo" src="assets/images/zelda.png" alt="Princess Zelda holding a sword">';
+            document.querySelector("#infoText").innerHTML = "<p>Princess Zelda plays the hero's counterpart in the Legend of Zelda series.  Following the first release in 1986, the series has consistenly produced games that are some of the most highly regarded in the industry.</p>";
+        }
+        if (word === 'pikachu'){
+            document.querySelector("#infoImage").innerHTML = '<img class="img-fluid nintendo" src="assets/images/pikachu.png" alt="Pikachu sitting">';
+            document.querySelector("#infoText").innerHTML = "<p>Pikachu was one of the primary charaters of the massively successful Pokemon TV show.  It began as a video game for the Game Boy, and as of today the series has sold more than 300 million copies.</p>";
+        }
+        if (word === 'bowser'){
+            document.querySelector("#infoImage").innerHTML = '<img class="img-fluid nintendo" src="assets/images/bowser.png" alt="Bowser in an aggressive stance">';
+            document.querySelector("#infoText").innerHTML = "<p>Bowser is the primary antagonist of the Mario series.  The standard plot involves him capturing a princess, and Mario coming to the rescue.</p>";
+        }
+        if (word === 'dedede'){
+            document.querySelector("#infoImage").innerHTML = '<img class="img-fluid nintendo" src="assets/images/dedede.png" alt="King Dedede holding a large mallet">';
+            document.querySelector("#infoText").innerHTML = "<p>King Dedede is the main antagonist of the Kirby series.  He's really just here because it's fun to have a 2-letter hangman game.</p>";
+        }
+        // if (word === 'pikmin'){
+        //     document.querySelector("#infoImage").innerHTML = '<img class="img-fluid nintendo" src="assets/images/pikmin.png" alt="A photo of Pikachu sitting">';
+        //     document.querySelector("#infoText").innerHTML = "<p>With the massively successful Pokemon Red & Blue, Pikachu became a gaming sensation beloved by children everywhere.</p>";
+        // }
+    }
+    
 // Play sound effects upon winning or losing
 
 
